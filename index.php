b3a06f4d48826cfadfc161fe3ff9ac980bdc8ea2
cactusjackk7 <cactusjackk7@gmail.com>
2021-05-17 19:08:32 +0200

    connected JS and css

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="nl">
+<head>
+   <title>Het Fruithuisje</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat" rel="stylesheet" type="text/css">
+   <link rel="stylesheet" href="assets/css/style.css">
+   
+</head>
+<body>
+<script src="app.js"></script>
+   
+</body>
+</html>
