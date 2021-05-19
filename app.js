91dbe7fc4ba43a28e16c2678443de7a353713b5a
cactusjackk7 <cactusjackk7@gmail.com>
2021-05-19 20:29:52 +0200

    added a function

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // JS part for the site
+
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+   
+ 	$(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+    if (this.hash !== "") {
+
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        window.location.hash = hash;
+      });
+    } 
+  });
+  
+   $(".slideanimate").each(function(){
+        $(this).stop().delay(1000).addClass("slide");
+    });
+  
+    $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+    
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass("slide");
+      }
+    });
+    });
+    
+})
+
+
+
+
